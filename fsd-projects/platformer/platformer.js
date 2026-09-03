ba02faff609df9ea9ca765b61562d58bb9cc8320
7029950-lab <7029950@apsb.us>
2026-09-03 19:29:03 +0000

    finished game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,28 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(550, 615, 100, 10,"hotpink")
+    createPlatform(800, 550, 100, 10,"teal")
+    createPlatform(1050, 490, 100, 10,"hotpink")
+    createPlatform(1225, 400, 100, 10, "teal")
+    createPlatform(620, 420, 100, 10, "hotpibk")
+    createPlatform(450, 310, 100, 10, "hotpink")
+    createPlatform(250, 385, 100, 10, "teal")
+    createPlatform(100, 290, 100 , 10, "hotpink")
+    
     // TODO 3 - Create Collectables
+    createCollectable("steve", 805, 500,)
+    createCollectable("diamond", 1285, 350)
+    createCollectable("diamond", 100, 200)
 
 
-
-    
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 700, 2000)
+    createCannon("left", 300, 4000)
+    createCannon("top", 1300, 2000)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
